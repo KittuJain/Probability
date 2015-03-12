ed01a31b2e70ebbafb19ebd83ee474b8adddf65e
Kittu Jain <cskratijain@gmail.com>
2015-03-12 13:40:04 +0530

    proability file refactored

--- src/com/probability/Probability.java
+++ src/com/probability/Probability.java
@@ -1,16 +1,43 @@
 package com.probability;
 
 public class Probability {
-    private double total_outcomes;
-    private double favourable_outcomes;
+    private double probability;
 
-    Probability(double favourable_outcomes, double total_outcomes){
-        this.favourable_outcomes = favourable_outcomes;
-        this.total_outcomes = total_outcomes;
+
+    public Probability(double probability) {
+        this.probability = probability;
     }
-    public Double calculateProbability() {
-        if(favourable_outcomes > total_outcomes)
+
+    public static Probability create(int favourableOutcomes, int allOutcomes) {
+        if(favourableOutcomes>allOutcomes || favourableOutcomes <0 || allOutcomes <0)
             return null;
-        return ((double)favourable_outcomes/total_outcomes);
+        return (new Probability((double)favourableOutcomes/allOutcomes));
+    }
+
+    public Probability and(Probability pbCoin2) {
+        return new Probability(probability*pbCoin2.probability);
+    }
+
+    public Probability or(Probability pbCoin2) {
+        Probability twoCoins = pbCoin2.and(new Probability(probability));
+        return new Probability(probability+pbCoin2.probability-twoCoins.probability);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Probability that = (Probability) o;
+
+        if (Double.compare(that.probability, probability) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        long temp = Double.doubleToLongBits(probability);
+        return (int) (temp ^ (temp >>> 32));
     }
 }
